db0064bb9962d7a220e11722ab2e23c0f97c7f6b
AkashSatla <satlaakash@gmail.com>
2022-03-02 09:26:19 +0530

    review

---
+++ array_sort.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+read -p "Give the size of array" n; 
+for (( i=0; i<n; i++ ))
+do
+
+	a=$(( RANDOM%900 + 100 ));
+	arr[i]=$a;
+done
+
+echo ${arr[@]};
+
+for (( p=0; p<n; p++ ))
+do 
+	for (( q=$p; q<n; q++ ))
+	do
+		if [ ${arr[$p]} -gt ${arr[$q]} ]
+		then
+			temp=${arr[$p]};
+			arr[$p]=${arr[$q]};
+			arr[$q]=$temp;
+		fi
+	done
+done
+
+
+echo ${arr[@]};
---
+++ fibo.sh
@@ -0,0 +1,20 @@
+	#!/bin/bash
+
+read -p "Enter a value of nth fibonacci number" n;
+
+f=0;
+s=1;
+
+for (( i=0; i<n;i++ ))
+do
+
+echo $f;
+t=$(( f + s ));
+f=$s;
+s=$t;
+
+done
+
+
+
+
